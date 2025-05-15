3b1f6dd82d639fdf2338bac6f66cb41c0d065688
Nick Schlecht <schlech3@msu.edu>
2025-05-15 16:57:23 -0400

    Edited the README (still needs examples put in). GCMS_Data_Visualizer.R received logic to detect the extension and currently will run .D through the rainbow parser and the CDF through ChromConverter. I attempted to open a .raw but it was not working and couldn't tell you for the life of me why not. I also tried to install entrab and realized it was not user friendly in the slightest.

--- GCMS_Data_Visualizer.R
+++ GCMS_Data_Visualizer.R
@@ -1,42 +1,32 @@
-# install.packages("remotes")
-# remotes::install_github("https://github.com/ethanbass/chromConverter/")
-
 require(chromConverter)
 require(tidyverse)
 require(scales)
+require(ncdf4)
 
-InputFileFolder <- 'C:/MSU/Coding/Stacked_Chromatogram_Generator/20230701/'    #Folder where .csv's are located
+InputFileFolder <- 'C:/MSU/Coding/Stacked_Chromatogram_Generator/Example_Data/'    #Folder where .csv's are located
 
 Process_Data <- TRUE         #If you are just finnicking with Plot parameters, set to FALSE to not have long wait period
 
-
-
                     #FileName,     #TIC/EIC mz,    +/- value i.e. ion = 40 RANGE = 0.5 aka 40 +/- 0.5. NA if TIC
 YourPlot <- tribble(~sample,       ~ion,           ~RANGE,   #Do not edit the  line
-                    '20230701_5-26-23_NBenthi_06.D', 'TIC', 'NA',
-                    '20230701_5-26-23_NBenthi_01.D', 'TIC', 'NA',
-                    '20230701_5-26-23_NBenthi_02.D', 'TIC', 'NA',
-                    '20230701_5-26-23_NBenthi_03.D', 'TIC', 'NA',
-                    '20230701_5-26-23_NBenthi_07.D', 'TIC', 'NA',
-                    '20230701_5-26-23_NBenthi_08.D', 'TIC', 'NA'
+                    'DXSGGPPS.D', 'TIC', 'NA',
+                    'DXSGGPPS_CamTPS2.D', 'TIC', 'NA'
 )
 
 DigitsBelowZero <- 1      #Relevant if you want to round up to the nearest m/z i.e. 45, 45.0, 45.00, etc. Stick to 0-3, but even 3 requires a lot of RAM
 
 Relative_OrCounts <- 'relative'     #Unless you want counts, do relative
 
-xrange <- c(5, 18)    #Pick x-axes range (minutes)
+xrange <- c(7, 20)    #Pick x-axes range (minutes)
 yrange <- c(0,1.3)    #if relative plot 0-1 or higher if stacking chroms
 
-
-VerticalSampleOffset <- 0.11     #How big of a gap between
+VerticalSampleOffset <- 0     #How big of a gap between
 HorizontalSampleOffset <- 0      #How much (time) to shift each additional stacked chromatogram by
 
 TickTimeGap <- 1           #Time gap between x-axis ticks
 Legend_WrapAmount <- 20    #How many characters before Legend goes to new line
 
-
-LineColors <- c('blue', 'orange','yellow','navy','darkgreen','red')    #Put in order of YourPlot variable. For pretty hexadecimals https://coolors.co/156064-00c49a-f8e16c-ffc2b4-fb8f67
+LineColors <- c('black', 'black')    #Put in order of YourPlot variable. For pretty hexadecimals https://coolors.co/156064-00c49a-f8e16c-ffc2b4-fb8f67
 
 ChromLineWidth <- 0.2      #How wide do you want your line? 
 
@@ -52,13 +42,12 @@ BorderLineWidth <- 0.4     #Determines how thick of lines you want for axes/box.
 
 plotdim <- c(8,5)     # Final Figure dimensions. Printed in R is not final dimensions so please check
 
-plotname <- 'TEST.png'      #Name the file. please include the type of file (.jpg .png, .svg). It will save in the same  Filefolder as input data.
-
-OutputFileFolder <- 'C:/MSU/Coding/Stacked_Chromatogram_Generator/20230701/'    #Folder where .csv's are located
+        plotname <- 'Stacked_TIC_DXSGGPPS_DXSGGPPS_CamTPS2.png'                        #Name the file. please include the type of file (.jpg .png, .svg). It will save in the same  Filefolder as input data.
+OutputFileFolder <- 'C:/MSU/Coding/Stacked_Chromatogram_Generator/Example_Data/'    #Folder where .csv's are located
 
 ############################################################
-#               DO NOT EDIT BELOW THIS LINE               #
-#       (Internal script logic for data handling/plotting)#
+#                DO NOT EDIT BELOW THIS LINE               #
+#     (Internal script logic for data handling/plotting)   #
 ############################################################
 
 #>>>>>>>>>>>>>>>>>>>>  Functions  <<<<<<<<<<<<<<<<<<<<<<<<<<
@@ -164,12 +153,26 @@ if(Process_Data == TRUE){
   gc()
   
   #initial data import
-  dat <- read_chroms(paste(InputFileFolder, YourPlot$sample, sep = ""), 
-                     format_in = "agilent_d",
-                     parser = "rainbow",
-                     format_out = 'data.table',
-                     data_format = "long"
-  )
+  
+  if(toupper(strsplit(YourPlot$sample, '\\.')[[1:2]]) == "D"){
+    dat <- read_chroms(paste(InputFileFolder, YourPlot$sample, sep = ""), 
+                       format_in = "agilent_d",
+                       parser = "rainbow",
+                       format_out = 'data.table',
+                       data_format = "long"
+    )
+  }
+  
+  if(toupper(strsplit(YourPlot$sample, '\\.')[[1:2]]) == "CDF"){
+    dat <- read_chroms(paste(InputFileFolder, YourPlot$sample, sep = ""), 
+                       format_in = "cdf",
+                       parser = "ChromConverter",
+                       format_out = 'data.table',
+                       data_format = "long"
+    )
+  }
+  
+
   
   gc()
   
@@ -204,14 +207,18 @@ if(Process_Data == TRUE){
   }
 }
 gc()
+
 # ----------------- End of Data Processing -----------------------
 
 #We don't remove ms_data3 in case we do "no processing"
+
+
 ms_data4 <- chrom_stack_adjuster(ms_data3, YourPlot, VerticalSampleOffset, HorizontalSampleOffset) #Adjusts the files in the order you showed them
 
 ms_data4
 ms_data4.0 <- ms_data4
-rm(ms_data4) %>% gc()
+rm(ms_data4)
+gc()
 
 ms_data4.0$sample <- factor(ms_data4.0$sample, levels = strip_extension(YourPlot$sample)) #Adds factor levels to the samples to put them in order you list them
 
@@ -236,7 +243,6 @@ Plot <- ms_data4.0 %>%
   scale_color_manual('sample', values = NamedColors, labels = wrapped_labels)
   # axis.line = element_line(color = "black", linewidth = 0.5)
 
-BorderLineWidth <- 0.4  
 
 Plot_Border_Type <- 'none'
   
--- README.md
+++ README.md
@@ -1,10 +1,55 @@
-# ReadMe.md
+# GCMS-Data-Visualizer
+A streamlined way to plot mass spectrometry data. 
+
+There's no manuscript to cite for GCMS-Data-Visualizer. Instead, simply cite the website as: Schlecht, N. https://github.com/schlech3/GCMS-Data-Visualizer and whichever software version you use.
+
+
+## Table of Contents: 
+1. Description
+2. Dependencies/Requirements
+3. Examples
+4. Other Scripts
+5. Author Notes
+   
 
 ## Description
-These scripts were initially designed as a straightforward approach to generating publication worthy chromatogram and spectra pictures by having a finer control over the figure generation. This not only includes text size, colors, line widths, alterations to x and y axis ranges, but you can also overlay several chromatograms and control if they are directly on top of one another, have a vertical shift between each, as well as includes horizontal shifts to offset them for a pseudo 3-D view. 
+These scripts were designed as a straightforward approach to generating publication worthy chromatograms with fine controls while requiring limited computational expertise. by having a finer control over the figure generation. The users have ready access to adjust text size, colors, line widths, alterations to x and y axis ranges. You can plot not only TICs, but XICs and are capable of overlaying several chromatograms and control the placement of these chromatograms, allowing you to have them directly overlayed, or vertically, and horizontally shifted them. The controlled stacking of them is particularly useful for comparing several chromatograms in a single figure. 
 
-## Necessary Data Prep
-Due to the proprietary nature of most GC datasets, you will need to export the data as a **CSV**. This should be done using the free and open-source software [Openchrom](https://www.openchrom.net/). Openchrom is a great tool for quickly looking at any dataset, but it is not suited for generating figures for publication. In openchrom you can export your files as a CSV which can be saved somewhere locally where you can use these scripts to load the data in and either generate your chromatogram(s) individually or in a combined figure, as well as plot any desired mass spectra. 
+The primary script is GCMS_Data_Visualizer.R . This can input direct data (i.e. Agilent's .D data format), processes them, and then takes your plot parameters and outputs your desired figure. I also have included older scripts that function quite differently  but may still have a niche usecase.
+
+## Dependencies, Requirements, and limitations
+
+### Dependencies 
+```
+require(chromConverter)
+require(tidyverse)
+require(scales)
+require(ncdf4)
+```
+
+A key aspect to streamlining data input was using Ethan Bass' [Chrom Converter](https://github.com/ethanbass/chromConverter/blob/master/README.md#Installation) to directly take mass spec data, as it is capable of converting it into a useful format. Thus far, I have automated .CDF and agilent .D files, however I have not had access to other instrument data, so either you can venture to install the correct parsers and add a new automation line yourself or leave a comment and you can send me the data so I can troubleshoot any new options. These parsers are almost certainly going to be the source of major bugs in the future. Though i can try to help on some of these, Ethan Bass and the other external parsers original creators are more likely able to resolve these issues than I so I encourage you to contact them about issues. 
+
+
+### Requirements/Limitations
+Though this program has no limit in the accuracy (how many decimal points you care to display), time range, or how many chromatograms you stack, RAM limitations will certainly come into play due to large generated tables. This is most relevant for HRMS, which may have issues loading several samples unless you have very impressive RAM. 
+
+
+For example: a 50-550 m/z with a 200ms scan time, 25 minute GC run with accuracy down to just first decimal point (i.e. table includes 50.0, 50.1, 50.2... 500) will generate a 37.5 million row data table using a minimum of 1Gb RAM. Current iterations actively remove memory as we filter your dataset to the relevant times, ions, etc. the intial loading of files 
+
+
+## Examples
+
+
+## Other Scripts
+
+I earlier alluded to my initial scripts, CSV_Chromatogram_generator.R and CSV_Spectra_generator.R
+
+These function quite similarly to the main script, GCMS_Data_Visualizer.R but require a **CSV** file as an input. I would obtain these CSVs by opening the mass spec file in the freely available software [Openchrom](https://www.openchrom.net/). From here you can export your chromatogram as a CSV and input it into these respective programs. 
+
+
+## Author notes
+Though 
+
 
 ## Current use case limitations
-The current iteration of these scripts are specifically designed for single quadrupole GC-MS data. These scripts utilize R 4.4.1 and the packages tidyverse, ggplot2, and scales. 
+The current iteration of these scripts are specifically designed for single quadrupole GC-MS data. These scripts utilize R 4.4.1 and the packages tidyverse, ggplot2, and scales.
