a179d70be23b8d37a7d34eb740f739f21baeb310
Nick Schlecht <schlech3@msu.edu>
2025-05-16 14:24:33 -0400

    After adding a loop to add chromatograms one at a time (less up front RAM usage leading to bottlenecks), I fixed a bug on the relative or absolute front. I then altered the plotting regarding the horizontal shift, where instead of the max x axis being your input range its your max rt, aka your x axis + the largest horizontal shift.

--- GCMS_Data_Visualizer.R
+++ GCMS_Data_Visualizer.R
@@ -5,30 +5,42 @@ require(ncdf4)
 
 InputFileFolder <- 'C:/MSU/Coding/Stacked_Chromatogram_Generator/Example_Data/'    #Folder where .csv's are located
 
+plotname <- 'Example4_VertHoriz_BKs.png'                               #Name the file. please include the type of file (.jpg .png, .svg). It will save in the same  Filefolder as input data.
+OutputFileFolder <- 'C:/MSU/Coding/Stacked_Chromatogram_Generator/Example_Data/'    #Folder where .csv's are located
+
 Process_Data <- TRUE         #If you are just finnicking with Plot parameters, set to FALSE to not have long wait period
 
                     #FileName,     #TIC/EIC mz,    +/- value i.e. ion = 40 RANGE = 0.5 aka 40 +/- 0.5. NA if TIC
 YourPlot <- tribble(~sample,       ~ion,           ~RANGE,   #Do not edit the  line
-                    'DXSGGPPS.D', 'TIC', 'NA',
-                    'DXSGGPPS_CamTPS2.D', 'TIC', 'NA'
+                    'DXSGGPPS_ArTPS2_AjReCYP76BK1.D', 'TIC', 'NA',
+                    'DXSGGPPS_ArTPS2_ClBuCYP76BK1.D', 'TIC', 'NA',
+                    'DXSGGPPS_ArTPS2_CoPy_A_CYP76BK1.D', 'TIC', 'NA',
+                    'DXSGGPPS_ArTPS2_HOSACYP76BK1.D', 'TIC', 'NA',
+                    'DXSGGPPS_ArTPS2_PeBaCYP76BK1.D', 'TIC', 'NA',
+                    'DXSGGPPS_ArTPS2_ScBaCYP76BK1.D', 'TIC', 'NA',
+                    'DXSGGPPS_ArTPS2_TeChCYP76BK1.D', 'TIC', 'NA',
+                    'DXSGPPS_ArTPS2_ViAgCYP76BK1.D', 'TIC', 'NA',
+                    'DXSGGPPS_ArTPS2_CaAmCYP76BK1.D', 'TIC', 'NA',
+                    'DXSGGPPS_ArTPS2.D', 'TIC', 'NA'
 )
 
 DigitsBelowZero <- 1      #Relevant if you want to round up to the nearest m/z i.e. 45, 45.0, 45.00, etc. Stick to 0-3, but even 3 requires a lot of RAM
 
 Relative_OrCounts <- 'relative'     #Unless you want counts, do relative
 
-xrange <- c(7, 20)    #Pick x-axes range (minutes)
-yrange <- c(0,1.3)    #if relative plot 0-1 or higher if stacking chroms
+xrange <- c(11.5, 17)    #Pick x-axes range (minutes)
+yrange <- c(0,1.9)    #if relative plot 0-1 or higher if stacking chroms
 
-VerticalSampleOffset <- 0     #How big of a gap between
-HorizontalSampleOffset <- 0      #How much (time) to shift each additional stacked chromatogram by
+VerticalSampleOffset <- 0.1     #How big of a gap between
+HorizontalSampleOffset <- 0.16      #How much (time) to shift each additional stacked chromatogram by
 
 TickTimeGap <- 1           #Time gap between x-axis ticks
 Legend_WrapAmount <- 20    #How many characters before Legend goes to new line
 
-LineColors <- c('black', 'black')    #Put in order of YourPlot variable. For pretty hexadecimals https://coolors.co/156064-00c49a-f8e16c-ffc2b4-fb8f67
 
-ChromLineWidth <- 0.2      #How wide do you want your line? 
+LineColors <- c('black', 'black', 'black', 'black', 'black', 'black','black','black','black','black')    #Put in order of YourPlot variable. For pretty hexadecimals https://coolors.co/156064-00c49a-f8e16c-ffc2b4-fb8f67
+
+ChromLineWidth <- 0.4      #How wide do you want your line? 
 
 xaxesLabelSize <- 14
 yaxesLabelSize <- 14
@@ -37,13 +49,11 @@ yaxesNumberSize <- 10
 LegendTitleSize <- 15
 LegendTextSize <- 12
 
-Plot_Border_Type <- "axes" # Options: "none", "axes", "box". This will determine if you want a border/axes.
+Plot_Border_Type <- "none" # Options: "none", "axes", "box". This will determine if you want a border/axes.
 BorderLineWidth <- 0.4     #Determines how thick of lines you want for axes/box.
 
-plotdim <- c(8,5)     # Final Figure dimensions. Printed in R is not final dimensions so please check
+plotdim <- c(9,5)     # Final Figure dimensions. Printed in R is not final dimensions so please check
 
-        plotname <- 'Stacked_TIC_DXSGGPPS_DXSGGPPS_CamTPS2.png'                        #Name the file. please include the type of file (.jpg .png, .svg). It will save in the same  Filefolder as input data.
-OutputFileFolder <- 'C:/MSU/Coding/Stacked_Chromatogram_Generator/Example_Data/'    #Folder where .csv's are located
 
 ############################################################
 #                DO NOT EDIT BELOW THIS LINE               #
@@ -139,7 +149,7 @@ chrom_stack_adjuster <- function(data, tbl, VerticalSampleOffset, HorizontalSamp
     mutate(
       rt = rt + HorizontalSampleOffset * (max(offset_index) - offset_index),
       Intensity = Intensity + VerticalSampleOffset * (max(offset_index) - offset_index)
-    )
+      ) 
 }
 
 #get_closest rt, strip_extension, summarise_mz, target_mz, normalize_column, chrom_stack_adjuster
@@ -149,77 +159,89 @@ chrom_stack_adjuster <- function(data, tbl, VerticalSampleOffset, HorizontalSamp
 
 #If statement allows skipping  data import and initial processing, which is quite slow.
 #There is regular clearing of memory and removal of variables at this stage due to table sizes.
+  #Initializing "final variable" from processing. (pre-vert/horizontal adjustment and plotting)
+
 if(Process_Data == TRUE){
   gc()
+  ms_data3.0 <- tibble()
+  #Need to loop through them 1 by 1 as importing all at once RAM intensive.
+  for(i in seq_len(nrow(YourPlot))){
+    chromatogram <- YourPlot %>% filter(row_number() == i)
+    print(paste("Processing", chromatogram$sample))
   
   #initial data import
-  
-  if(toupper(strsplit(YourPlot$sample, '\\.')[[1:2]]) == "D"){
-    dat <- read_chroms(paste(InputFileFolder, YourPlot$sample, sep = ""), 
-                       format_in = "agilent_d",
-                       parser = "rainbow",
-                       format_out = 'data.table',
-                       data_format = "long"
-    )
-  }
-  
-  if(toupper(strsplit(YourPlot$sample, '\\.')[[1:2]]) == "CDF"){
-    dat <- read_chroms(paste(InputFileFolder, YourPlot$sample, sep = ""), 
-                       format_in = "cdf",
-                       parser = "ChromConverter",
-                       format_out = 'data.table',
-                       data_format = "long"
-    )
-  }
-  
-
-  
-  gc()
-  
-  #transform into useful tibble
-  ms_data <- imap_dfr(dat, ~ {
-    .x$MS %>%
-      as_tibble() %>%
-      mutate(sample = .y)
-  })
-  
-  rm(dat)
-  gc()
-   
-  #limits to your desired range, removes 0 intensity ions, rounds to the desired sigfig.
-  ms_data2 <- ms_data %>%
-    summarise_mz(. , DigitsBelowZero)
-  
-  rm(ms_data)
-  gc()
-  
-  # pulls only desired XIC or calculates the TIC you want. 
-  ms_data3 <- ms_data2 %>%
-    target_mz(. , YourPlot)
-  
-  
-  rm(ms_data2)
-  gc()
-  
-  #Converts to relative (1 = largest TIC/XIC in whole dataset)
-  if (tolower(Relative_OrCounts) == 'relative' ){
-   ms_data3 <- normalize_column(ms_data3)
-  }
+    if(toupper(strsplit(chromatogram$sample, '\\.')[[1:2]]) == "D"){
+      dat <- read_chroms(paste(InputFileFolder, chromatogram$sample, sep = ""), 
+                         format_in = "agilent_d",
+                         parser = "rainbow",
+                         format_out = 'data.table',
+                         data_format = "long"
+      )
+    }
+    
+    if(toupper(strsplit(chromatogram$sample, '\\.')[[1:2]]) == "CDF"){
+      dat <- read_chroms(paste(InputFileFolder, chromatogram$sample, sep = ""), 
+                         format_in = "cdf",
+                         parser = "ChromConverter",
+                         format_out = 'data.table',
+                         data_format = "long"
+      )
+    }
+    gc()
+    
+    #transform into useful tibble
+    ms_data <- imap_dfr(dat, ~ {
+      .x$MS %>%
+        as_tibble() %>%
+        mutate(sample = .y)
+    })
+    
+    rm(dat)
+    gc()
+     
+    #limits to your desired range, removes 0 intensity ions, rounds to the desired sigfig.
+    ms_data2 <- ms_data %>%
+      summarise_mz(. , DigitsBelowZero)
+    
+    rm(ms_data)
+    gc()
+    
+    # pulls only desired XIC or calculates the TIC you want. 
+    ms_data3 <- ms_data2 %>%
+      target_mz(. , chromatogram) 
+    
+    
+    rm(ms_data2)
+    gc()
+    
+    ms_data3.0 <- rbind(ms_data3.0, ms_data3)
+    
 }
 gc()
 
+
+if (tolower(Relative_OrCounts) == 'relative' ){
+  ms_data3.0 <- normalize_column(ms_data3.0)
+}
 # ----------------- End of Data Processing -----------------------
+
+#Converts to relative (1 = largest TIC/XIC in whole dataset)
+
+}
+
+
+
+
 
 #We don't remove ms_data3 in case we do "no processing"
 
+ms_data4 <- chrom_stack_adjuster(ms_data3.0, YourPlot, VerticalSampleOffset, HorizontalSampleOffset) #Adjusts the files in the order you showed them
 
-ms_data4 <- chrom_stack_adjuster(ms_data3, YourPlot, VerticalSampleOffset, HorizontalSampleOffset) #Adjusts the files in the order you showed them
 
-ms_data4
 ms_data4.0 <- ms_data4
 rm(ms_data4)
 gc()
-
+ms_data4.0
 ms_data4.0$sample <- factor(ms_data4.0$sample, levels = strip_extension(YourPlot$sample)) #Adds factor levels to the samples to put them in order you list them
 
 NamedColors <- setNames(LineColors[1:nrow(YourPlot)], levels(YourPlot$sample))  #ensures that the colors match the above color options
@@ -231,7 +253,7 @@ Plot <- ms_data4.0 %>%
   ylim(c(yrange[1], yrange[2])) +
   ylab('Intensity') +
   xlab('minutes') +
-  scale_x_continuous(limits = c(xrange[1], xrange[2]), breaks = seq(from = omnibus::roundTo(xrange[1],TickTimeGap), to = omnibus::roundTo(xrange[2],TickTimeGap), by = TickTimeGap)) +
+  scale_x_continuous(limits = c(xrange[1], max(ms_data4.0$rt), breaks = seq(from = omnibus::roundTo(xrange[1],TickTimeGap), to = omnibus::roundTo(xrange[2],TickTimeGap), by = TickTimeGap))) +
   theme(panel.background = element_blank(),
         axis.text.x = element_text(size = xaxesNumberSize),
         axis.text.y = element_text(size = yaxesNumberSize),
@@ -243,9 +265,6 @@ Plot <- ms_data4.0 %>%
   scale_color_manual('sample', values = NamedColors, labels = wrapped_labels)
   # axis.line = element_line(color = "black", linewidth = 0.5)
 
-
-Plot_Border_Type <- 'none'
-  
 if(tolower(Plot_Border_Type) == 'box' ){
   Plot <- Plot + 
     theme(panel.border = element_rect(color = 'black', fill = NA, linewidth = BorderLineWidth))
@@ -253,12 +272,12 @@ if(tolower(Plot_Border_Type) == 'box' ){
 
 if(tolower(Plot_Border_Type) == 'axes' ){
   Plot <- Plot + 
-    theme(    axis.line = element_line(color = "black", linewidth = BorderLineWidth))
+    theme(axis.line = element_line(color = "black", linewidth = BorderLineWidth))
 }
 
-
-print(Plot)
+suppressWarnings(print(Plot))
 
 OUT <- paste(OutputFileFolder, plotname, sep = '')
 ggsave(OUT, plot = Plot, width = plotdim[1], height = plotdim[2], bg = 'transparent' )
 rm(Plot)
+gc()
